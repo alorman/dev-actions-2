42c25b19ee59640c2bfd6f9616f9f63f1f3fe978
Alex Lorman <alex@alexlorman.com>
2024-03-14 11:59:21 -0400

    Update copy-scripting.sh

--- copy-scripting.sh
+++ copy-scripting.sh
@@ -1,6 +1,6 @@
 find -maxdepth 1 -type f -iname readme.md -exec cat .github/scripts/header.txt {} > tempreadme.md \;
 echo "Found Readme and copied"
-cat .github/scripts/footer.txt >> tempreadme.md
+cat footer.txt >> tempreadme.md
 echo "copied footer"
 find -maxdepth 1 -type f -iname readme.md -exec cp tempreadme.md {} \;
 echo "move success"
