1077dc1827e2ccd975d4a5e22c6900aa2c3a7ddc
Alex Lorman <alex@alexlorman.com>
2024-03-14 22:04:51 -0400

    Update copy-scripting.sh

--- scripts/copy-scripting.sh
+++ scripts/copy-scripting.sh
@@ -7,3 +7,4 @@ cat docs-bot/markdown/footer.txt >> tempreadme.md
 echo "copied footer"
 find -maxdepth 1 -type f -iname readme.md -exec cp tempreadme.md {} \;
 echo "move success"
+echo "readback $IMAGE_NAME and $PROJECT_CATEGORY"
