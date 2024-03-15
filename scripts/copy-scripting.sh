bafb0ff51c786f240244b8f2fd260a319d10561c
Alex Lorman <alex@alexlorman.com>
2024-03-14 22:40:52 -0400

    Update copy-scripting.sh

--- scripts/copy-scripting.sh
+++ scripts/copy-scripting.sh
@@ -2,6 +2,9 @@ find -maxdepth 1 -type f -iname readme.md -exec cat docs-bot/markdown/header.txt
 echo "Found Readme and copied"
 cat docs-bot/markdown/footer.txt >> tempreadme.md
 echo "copied footer"
+sed -i 's/$IMAGE_NAME_LINK/$IMAGE_NAME/g' tempreadme.txt
+sed -i 's/$PROJECT_CATEGORY_LINK/$PROJECT_CATEGORY/g' tempreadme.txt
+echo "insert variable names $IMAGE_NAME and $PROJECT_CATEGORY"
 find -maxdepth 1 -type f -iname readme.md -exec cp tempreadme.md {} \;
 echo "move success"
 echo "readback $IMAGE_NAME and $PROJECT_CATEGORY"
